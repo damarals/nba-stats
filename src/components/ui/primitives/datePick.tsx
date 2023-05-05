60ef8736cf5ab32b587a5294d275a6dbf8832e08
Daniel Amaral <silva.daniel86@gmail.com>
2023-05-05 19:05:56 -0300

    :art: update prettier config

--- src/components/ui/primitives/datePick.tsx
+++ src/components/ui/primitives/datePick.tsx
@@ -1,26 +1,20 @@
-"use client";
+"use client"
 
-import * as React from "react";
-import { addDays, format } from "date-fns";
-import { Calendar as CalendarIcon } from "lucide-react";
-import { type DateRange } from "react-day-picker";
+import * as React from "react"
+import { cn } from "@/utils/cn"
+import { addDays, format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+import { type DateRange } from "react-day-picker"
 
-import { cn } from "@/utils/cn";
-import { Button } from "@/components/ui/primitives/button";
-import { Calendar } from "@/components/ui/primitives/calendar";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/primitives/popover";
+import { Button } from "@/components/ui/primitives/button"
+import { Calendar } from "@/components/ui/primitives/calendar"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/primitives/popover"
 
-export function CalendarDateRangePicker({
-  className,
-}: React.HTMLAttributes<HTMLDivElement>) {
+export function CalendarDateRangePicker({ className }: React.HTMLAttributes<HTMLDivElement>) {
   const [date, setDate] = React.useState<DateRange | undefined>({
     from: new Date(2022, 0, 20),
     to: addDays(new Date(), -1),
-  });
+  })
 
   return (
     <div className={cn("grid gap-2", className)}>
@@ -29,17 +23,13 @@ export function CalendarDateRangePicker({
           <Button
             id="date"
             variant={"outline"}
-            className={cn(
-              "justify-start text-left font-normal",
-              !date && "text-muted-foreground"
-            )}
+            className={cn("justify-start text-left font-normal", !date && "text-muted-foreground")}
           >
             <CalendarIcon className="mr-2 h-4 w-4" />
             {date?.from ? (
               date.to ? (
                 <>
-                  {format(date.from, "LLL dd, y")} -{" "}
-                  {format(date.to, "LLL dd, y")}
+                  {format(date.from, "LLL dd, y")} - {format(date.to, "LLL dd, y")}
                 </>
               ) : (
                 format(date.from, "LLL dd, y")
@@ -61,5 +51,5 @@ export function CalendarDateRangePicker({
         </PopoverContent>
       </Popover>
     </div>
-  );
+  )
 }
