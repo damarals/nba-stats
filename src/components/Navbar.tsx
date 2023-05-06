1c82220d6dc2e82c304375e7d8703df81e8dd979
Daniel Amaral <silva.daniel86@gmail.com>
2023-05-06 12:57:27 -0300

    :art: improve navbar style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,60 +17,58 @@ import { Separator } from "@/components/ui/primitives/separator"
 
 export default function Navbar() {
   return (
-    <nav className="row-span-1 flex items-center justify-end space-x-3 p-4">
+    <nav className="row-span-1 flex items-center justify-end p-4">
       <Popover>
         <PopoverTrigger>
           <Button variant="ghost">
             <BellIcon size={25} />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-64" sideOffset={10} align="end">
-          <div className="text-center">Notificações</div>
+        <PopoverContent className="w-56" sideOffset={10} align="end">
+          <div className="text-center font-medium text-sm">Notificações</div>
           <Separator />
           <div className="mt-4 flex flex-col items-center space-y-3">
-            <InboxIcon size={60} className="stroke-slate-600" strokeWidth={1.5} />
-            <p className="text-sm font-light">
+            <InboxIcon size={40} className="stroke-slate-600" strokeWidth={1.5} />
+            <p className="text-xs font-light text-center">
               Sem notificações no momento, você está livre para explorar a plataforma!
             </p>
           </div>
         </PopoverContent>
       </Popover>
-      <div className="flex">
-        <DropdownMenu modal={false}>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="hover:bg-transparent p-0">
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-44" sideOffset={10} align="end">
-            <DropdownMenuLabel className="font-normal">
-              <div className="flex flex-col space-y-1">
-                <p className="text-sm font-medium leading-none">daniels</p>
-                <p className="text-xs leading-none text-muted-foreground">daniels@example.com</p>
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <Settings className="mr-2 h-4 w-4" />
-                <Link href="#" passHref>
-                  Configurações
-                </Link>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="hover:bg-transparent">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-44" sideOffset={10} align="end">
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">daniels</p>
+              <p className="text-xs leading-none text-muted-foreground">daniels@example.com</p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
             <DropdownMenuItem>
-              <LogOut className="mr-2 h-4 w-4" />
+              <Settings className="mr-2 h-4 w-4" />
               <Link href="#" passHref>
-                Sair
+                Configurações
               </Link>
             </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <LogOut className="mr-2 h-4 w-4" />
+            <Link href="#" passHref>
+              Sair
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </nav>
   )
 }
