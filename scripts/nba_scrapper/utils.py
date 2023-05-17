05161fe5b29004410395ec33f3dca71741f9e30d
Daniel Amaral <silva.daniel86@gmail.com>
2023-05-17 14:34:23 -0300

    :bug: try to fix gh actions

--- scripts/nba_scrapper/utils.py
+++ scripts/nba_scrapper/utils.py
@@ -2,13 +2,13 @@ import time
 import pandas as pd
 
 nba_headers = {
-    'Host': 'stats.nba.com',
-    'Connection': 'keep-alive',
-    'Accept': 'application/json, text/plain, */*',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
-    'Referer': 'https://stats.nba.com/',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Host': 'stats.nba.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
 }
 
 def convert_height(height: str) -> float:
