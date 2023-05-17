36c1164cd0b0bf1759d065742b08373659928467
Daniel Amaral <silva.daniel86@gmail.com>
2023-05-17 14:56:26 -0300

    :bug: try to fix gh actions

--- scripts/nba_scrapper/utils.py
+++ scripts/nba_scrapper/utils.py
@@ -2,20 +2,17 @@ import time
 import pandas as pd
 
 nba_headers = {
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'pt-BR,pt;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-    'Connection': 'keep-alive',
-    'Host': 'stats.nba.com',
-    'Origin': 'https://www.nba.com',
-    'Referer': 'https://www.nba.com/',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-site',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.42',
-    'sec-ch-ua': '"Microsoft Edge";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
+    'Host': "stats.nba.com", 
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0", 
+    'Accept': "application/json, text/plain, */*", 
+    'Accept-Language': "en-US,en;q=0.5", 
+    'Accept-Encoding': "gzip, deflate, br", 
+    'x-nba-stats-origin': "stats", 
+    'x-nba-stats-token': "true", 
+    'Connection': "keep-alive", 
+    'Referer': "https://www.nba.com/", 
+    'Pragma': "no-cache", 
+    'Cache-Control': "no-cache"
 }
 
 def convert_height(height: str) -> float:
